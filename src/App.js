3c3a0d52858e884a4d275516784e1bc4f5973b9c
icedjello <bamfard@gmail.com>
2020-02-18 16:23:27 +0000

    Run & Stop working. Styling moved to styles.css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import { AgGridReact } from 'ag-grid-react';
@@ -6,10 +6,19 @@ import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-balham-dark.css';
 import 'ag-grid-enterprise';
 
+import './styles.css'
+
 import { startTime } from './store/reducer';
 import * as actions from './store/actions';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+    }
+    this.intervalUpdater = null;
+  }
 
   onGridReady(params) {
     this.gridApi = params.api;
@@ -18,27 +27,24 @@ class App extends Component {
   }
 
   advanceButtonHandler = () => {
-    this.props.onAdvance();
+    this.props.onAdvance()
   }
 
   runButtonHandler = () => {
-    this.props.onRun();
+    this.intervalUpdater = setInterval(this.props.onAdvance, 300)
   }
 
   stopButtonHandler = () => {
-    this.props.onStop();
+    clearInterval(this.intervalUpdater);
   }
 
   render() {
     return (
-      <Fragment>
-
+      <div
+        className='parent'
+      >
         <div
           className='ag-theme-balham-dark'
-          style={{
-            width: '100%',
-            height: '900px'
-          }}
         >
           <AgGridReact
             columnDefs={[
@@ -94,43 +100,42 @@ class App extends Component {
             getRowNodeId={data => data.id}>
           </AgGridReact>
         </div>
+
         <div
-          style={{
-            margin: '5px'
-          }}
+          className='button-container'
         >
-          <button onClick={this.advanceButtonHandler}>Advance</button>
+          <div
+            className='cremeButton-wrap'
+          >
+            <button
+              className='cremeButton'
+              onClick={this.advanceButtonHandler}
+            >
+              Advance</button>
+            <button
+              className='cremeButton'
+              onClick={this.runButtonHandler}
+            >
+              Run</button>
+            <button
+              className='cremeButton'
+              onClick={this.stopButtonHandler}
+            >
+              Stop</button>
+          </div>
         </div>
-        <div
-          style={{
-            margin: '5px'
-          }}
-        >
-          <button onClick={this.runButtonHandler}>Run</button>
-          <button onClick={this.runButtonHandler}>Stop</button>
-        </div>
-      </Fragment>
+
+
+      </div>
     );
   }
 }
 
-
-
-
 const _preFormatter = (params, formatter) => params.node.group ? formatter(params.value) : formatter(params.data[params.column.colDef.field])
 
-
-// params.node.group ? formatter(params.value) : 
-
-
 const _priceFormatter = value => (Math.round(value * 100) / 100).toFixed(2)
-// const _priceFormatter = value => 'hello'
-
 
 const _bskFormatter = value => value === 0 ? 'BUY' : value === 1 ? 'SELL' : 'KEEP'
-// const _bskFormatter = value => 'hello'
-
-
 
 const mapStateToProps = state => {
   return {
